e510b188ebc10b3b06125dc976f7550cb01ed4dc
Joanna <joanna.gargas@gmail.com>
2018-02-11 15:53:11 +0100

    Add main, numberToGuess field and generate numberToGuess method

--- src/quessNum/Game.java
+++ src/quessNum/Game.java
@@ -2,4 +2,14 @@ package quessNum;
 
 
 public class Game {
+    private int numberToGuess;
+
+    private void generateRandomNumberToGuess() {
+        numberToGuess = (int) (Math.random()*100) + 1;
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.generateRandomNumberToGuess();
+    }
 }
